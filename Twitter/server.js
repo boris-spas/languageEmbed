024591b76c7c0523e22072a369ef6b90e76f5dfc
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-05-24 18:12:07 +0200

    To chrash a vm.

--- Twitter/server.js
+++ Twitter/server.js
@@ -14,42 +14,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
-var form ='';
-    form += "<form action='/sentiment'  method='post' name='form1'>";
-    form += "Twitter Account Name:<input type= 'text' name='name'></p>";
-    form += "Number of Tweets:<input type='text' name='number'></p>";
-    form += "<input type='submit' value='submit'>";
-    form += "</form>";
-
-
-app.get('/sentiment', (req, res) => {
-    res.send(form);
-  });
-
-app.post('/sentiment', urlencodedParser ,(req, res) => {
-    console.time('rAnalysis');
-
-    if(req.body.name && req.body.number && /^\d+$/.test(req.body.number)){
-        var searchTerm = req.body.name;
-        var tweetCount = parseInt(req.body.number);
-        var rPlot = JavaHost.tweetSentiment(searchTerm, tweetCount);
-        res.send(form + rPlot);
-    }
-    else {
-        res.send(form + "false input");
-    }
-
-    console.timeEnd('rAnalysis');
+app.get('/', () => {
+     var resp = JavaHost.tweetSentiment("@scgbern", 100);
+     res.send(resp);
 });
-
-app.get('/sentimentjson', () => {
-
-    var json = {"searchTerm":"@scgbern", "tweetCount":100};
-    var rPlot = JavaHost.tweetSentiment(json);
-    res.send(rPlot);
-
-});
-
 
 var port = 3000;
 var server = app.listen(port, function() {
