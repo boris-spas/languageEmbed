6f6c36ff8a12d5a2b65f71f25dc86a83663f8b71
FLurinT <flurin.truebner@students.unibe.ch>
2018-06-04 14:58:21 +0200

    reformated java, ruby + R files

--- Twitter/sentimentAnalysis.r
+++ Twitter/sentimentAnalysis.r
@@ -1,44 +1,41 @@
 library(stringr)
 library(lattice)
 
-wordsInString <- function(tweet){
-    tweet = gsub('https://','',tweet)
-    tweet = gsub('http://','',tweet)
-    tweet = gsub('[^[:graph:]]', ' ',tweet)
-    tweet = gsub('[[:punct:]]', '', tweet)
-    tweet = gsub('[[:cntrl:]]', '', tweet)
-    tweet = gsub('\\d+', '', tweet)
-    tweet = str_replace_all(tweet,"[^[:graph:]]", " ") 
-    tweet = tolower(tweet)
-
-    word.list = str_split(tweet, '\\s+')
-    words = unlist(word.list)
-
-    return(words)
+wordsInString <- function(tweet) {
+  tweet = gsub('https://', '', tweet)
+  tweet = gsub('http://', '', tweet)
+  tweet = gsub('[^[:graph:]]', ' ', tweet)
+  tweet = gsub('[[:punct:]]', '', tweet)
+  tweet = gsub('[[:cntrl:]]', '', tweet)
+  tweet = gsub('\\d+', '', tweet)
+  tweet = str_replace_all(tweet, "[^[:graph:]]", " ")
+  tweet = tolower(tweet)
+  
+  word.list = str_split(tweet, '\\s+')
+  words = unlist(word.list)
+  
+  return(words)
 }
 
-sentimentAnalysis <- function(tweets){
-    scores <- c()
-
-    neg = scan("negativeWords.txt", what="character", quiet = TRUE)
-    pos = scan("positiveWords.txt", what="character", quiet = TRUE)
-
-    for(i in 1:length(tweets)){
-        words <- tweets[i]
-
-        pos.matches = match(words, pos)
-        neg.matches = match(words, neg)
- 
-        pos.matches = !is.na(pos.matches)
-        neg.matches = !is.na(neg.matches)
- 
-        score = sum(pos.matches) - sum(neg.matches)
-
-        scores <- c(scores, score)
-    }
-
-    svg(width=4, height=3)
-    print(histogram(scores));
-    grDevices:::svg.off()
+sentimentAnalysis <- function(tweets) {
+  scores <- c()
+  
+  neg = scan("negativeWords.txt", what = "character", quiet = TRUE)
+  pos = scan("positiveWords.txt", what = "character", quiet = TRUE)
+  
+  for (i in 1:length(tweets)) {
+    words <- wordsInString(tweets[i])
+    
+    pos.matches = match(words, pos)
+    neg.matches = match(words, neg)
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    score = sum(pos.matches) - sum(neg.matches)
+    scores <- c(scores, score)
+  }
+  
+  svg(width = 4, height = 3)
+  print(histogram(scores))
+  grDevices:::svg.off()
 }
-
--- Twitter/twitterGet.rb
+++ Twitter/twitterGet.rb
@@ -1,33 +1,32 @@
-require 'twitter'
+require "twitter"
 
 def getTweets(searchTerm, tweetCount)
-  begin   
+  begin
     data = Hash.new
-    File.readlines(File.join(File.expand_path(File.dirname(__FILE__)), 'twitterToken.txt')).each do |line|
-      var,val = line.chomp.split("=")
+    File.readlines(File.join(File.expand_path(File.dirname(__FILE__)), "twitterToken.txt")).each do |line|
+      var, val = line.chomp.split("=")
       data[var] = val
     end
 
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = data['consumer_key']
-      config.consumer_secret     = data['consumer_secret']
-      config.access_token        = data['access_token']
-      config.access_token_secret = data['access_token_secret']
+      config.consumer_key = data["consumer_key"]
+      config.consumer_secret = data["consumer_secret"]
+      config.access_token = data["access_token"]
+      config.access_token_secret = data["access_token_secret"]
     end
 
     twitterData = Array.new
 
-    if searchTerm[0] == '@'
+    if searchTerm[0] == "@"
       tweets = client.user_timeline(searchTerm, count: tweetCount)
-    else 
+    else
       tweets = client.search(searchTerm, lang: "en").take(tweetCount)
     end
 
-    tweets.each { |tweet| twitterData.push(tweet.full_text)}
+    tweets.each { |tweet| twitterData.push(tweet.full_text) }
     twitterData
-
   rescue => e
-    raise StandardError.new(e.inspect.tr('>', '').tr('<', '') << "<br>" << e.backtrace.join("\n"))
+    raise StandardError.new(e.inspect.tr(">", "").tr("<", "") << "<br>" << e.backtrace.join("\n"))
   end
 end
 
